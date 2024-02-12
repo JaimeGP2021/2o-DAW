cd93b93a0bf48660eb11b4fc742a27d5daf9e9f1
Jaime García <jaime.garcia@iesdonana.org>
2024-02-12 19:58:18 +0100

    Ejercicio 3 rafascritp

--- Desarrollo Web en Entorno Cliente/UD7. Comunicación Asíncrona/Tareas/03_encuesta.js
+++ Desarrollo Web en Entorno Cliente/UD7. Comunicación Asíncrona/Tareas/03_encuesta.js
@@ -2,12 +2,26 @@ document.addEventListener("DOMContentLoaded", function () {
   var votos = document.querySelectorAll(".voto");
   votos.forEach((voto) => {
     voto.addEventListener("click", function () {
-      console.log(voto);
-      console.log(voto.value);
       fetch("03_encuesta_voto.php?input=" + voto.value)
-        .then((result) => {})
-        .catch((err) => {});
+        .then(function (result) {
+          return result.text();
+        })
+        .then(function (data) {
+          pintavotos(data);
+        });
     });
   });
-  console.log(response);
+
+  function pintavotos(votaciones) {
+    let array = votaciones.split("||");
+    let total =
+      parseInt(array[0]) +
+      parseInt(array[1]) +
+      parseInt(array[2]) +
+      parseInt(array[3]);
+    for (let i = 0; i < 4; i++) {
+      document.getElementById(`${i}`).value =
+        (parseInt(array[i]) / total) * 100;
+    }
+  }
 });
--- Desarrollo Web en Entorno Cliente/UD7. Comunicación Asíncrona/Tareas/03_encuesta_voto.php
+++ Desarrollo Web en Entorno Cliente/UD7. Comunicación Asíncrona/Tareas/03_encuesta_voto.php
@@ -2,12 +2,10 @@
 $equipostxt = fopen('03_resultados.txt', 'r');
 $primeralinea = fgets($equipostxt);
 $equiposarray = explode('||', $primeralinea);
-$equiposarray[$_GET('input')] = $equiposarray[$_GET('input')] + 1;
+$equiposarray[$_GET['input']] += 1;
 $primeralinea = implode('||', $equiposarray);
 
 echo $primeralinea;
 
 $nuevoarchivo = fopen('03_resultados.txt', 'w');
 fwrite($nuevoarchivo, $primeralinea);
-
-?>
