b3402c349bfe1da5aeb551c04ba4d3b27bb1b738
Jaime García <jaime.garcia@iesdonana.org>
2024-04-01 21:18:40 +0200

    Añadido al dashboard videojuegos

--- Desarrollo Web en Entorno Servidor/junio/jun-videojuegos/resources/views/layouts/navigation.blade.php
+++ Desarrollo Web en Entorno Servidor/junio/jun-videojuegos/resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('videojuegos.index')" :active="request()->routeIs('videojuegos.index')">
+                        {{ __('Videojuegos') }}
+                    </x-nav-link>
                 </div>
             </div>
 
