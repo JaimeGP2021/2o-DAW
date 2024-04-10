88723169441e79a97c2b2bb61d0aaf7205689227
Jaime García <jaime.garcia@iesdonana.org>
2024-04-10 18:54:20 +0200

    Acordeón ejemplo listo

--- Diseño de Interfaces Web/UD4. Integración de Contenido Interactivo/Acordeón/acordeon.js
+++ Diseño de Interfaces Web/UD4. Integración de Contenido Interactivo/Acordeón/acordeon.js
@@ -1,14 +1,13 @@
 const bloque    = document.querySelectorAll('.bloque')
 const h2        = document.querySelectorAll('.h2')
-
 //Cuando haga click en h2,
     // QUITAR la clase activo de TODOS los bloques
     // Vamos a añadir la clase activo al BLOQUE con la POSICION del h2
 
-h2.foreach( (cadaH2, i ) => {
+h2.forEach( (cadaH2, i ) => {
     h2[i].addEventListener('click', ()=>{
 
-        bloque.foreach( (cadaBloque, i)=>{
+        bloque.forEach( (cadaBloque, i)=>{
             bloque[i].classList.remove('activo')
         })
         bloque[i].classList.add('activo')
