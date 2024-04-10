2a6343d8baa6830c9229aa6512ba51c2a1fc77d8
Jaime García <jaime.garcia@iesdonana.org>
2024-04-10 19:34:06 +0200

    Route pendiente

--- Desarrollo Web en Entorno Cliente/UD8. Framework-ReactJS/Tareas/prueba/src/pages/App.jsx
+++ Desarrollo Web en Entorno Cliente/UD8. Framework-ReactJS/Tareas/prueba/src/pages/App.jsx
@@ -1,15 +1,26 @@
 import Header from '../components/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from '../components/Footer';
 //import "/src/style/style.css";
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
+      <Routes>
       <Header />
-
+      <>
+        <hr></hr>
+        <hr></hr>
+        <hr></hr>
+        <hr></hr>
+        <hr></hr>
+        <hr></hr>
+        <hr></hr>
+      </>
       <Footer />
-    </>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
