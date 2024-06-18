b373c20b7d4ddebefad6862295d3f75452cc7c29
Jaime García <jaime.garcia@iesdonana.org>
2024-06-18 16:17:42 +0200

    Inicio nuevo, vaya liada

--- Desarrollo Web en Entorno Servidor/junio/Mercadonald/routes/web.php
+++ Desarrollo Web en Entorno Servidor/junio/Mercadonald/routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::view('/', 'cajaAmiga.empezar');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
